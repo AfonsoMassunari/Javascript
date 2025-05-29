bb6816a3a47ce6b1f05d108908b8f55f993f3a73
Afonso Henriques Massunari <96322732+AfonsoMassunari@users.noreply.github.com>
2025-05-29 12:09:06 -0300

    Add files via upload

---
+++ Javascript Studies/4 DOM and Events/Project 1 Guess My Number/script.js
@@ -0,0 +1,77 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+// O querySelector retorna o elemento selecionado .textContent mostra o conteúdo dentro do elemento
+//Document is the entry point to the DOM
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+//HANDLING CLICK EVENTS
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //When is no input
+  if (!guess) {
+    displayMessage('⛔ No Number!');
+  }
+
+  //When player wins
+  else if (guess == secretNumber) {
+    displayMessage('🎉 Correct Number!');
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+
+  //When is too low or too high
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('🙁 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+// addEventListener to attach an event handler,and that event handler method receive a function as an argument
+//This function will only be called when the event happens
+
+//Challenge #1
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
